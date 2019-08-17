537e94fdfbf40f30fe921ab61ea71116d4cdc9a1
Marco Vargas <mav89ls@gmail.com>
2019-08-17 10:39:17 -0700

    added code for styled-components to clean up the mess in the episodes tab

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
 export default function EpisodeCard(props) {
   return (
-    <span className="episode-card">
-      <h2>{props.name}</h2>
-      <h3>Episode number: {props.number}</h3>
-      <p>Air Date: {props.airDate}</p>
-    </span>
+    <Card className="episode-card">
+      <Card.Content>
+        <Card.Header>{props.name}</Card.Header>
+        <Card.Description>Episode number: {props.number}</Card.Description>
+        <Card.Description>Air Date: {props.airDate}</Card.Description>
+      </Card.Content>
+    </Card>
   );
 }
