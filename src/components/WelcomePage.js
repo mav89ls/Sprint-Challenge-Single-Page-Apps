a302dd4a2d48133bf3af4119f761c6e85226a3cb
Marco Vargas <mav89ls@gmail.com>
2019-08-17 10:44:08 -0700

    added better homepage picture

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -7,7 +7,7 @@ export default function WelcomePage() {
         <h1>Welcome to the ultimate fan site!</h1>
         <img
           className="main-img ui centered medium circular image"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          src="https://39m9vk1z5i3x15rspj43y7k8-wpengine.netdna-ssl.com/wp-content/uploads/2018/05/rick-and-morty-renewed-1300x731.jpg"
           alt="rick"
         />
       </header>
