8a1ef2c2aac3d90aedd425d95b7f9fe58904b658
Marco Vargas <mav89ls@gmail.com>
2019-08-17 11:03:11 -0700

    built search bar

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -8,7 +8,7 @@ export default function WelcomePage() {
         <img
           className="main-img ui centered medium circular image"
           src="https://39m9vk1z5i3x15rspj43y7k8-wpengine.netdna-ssl.com/wp-content/uploads/2018/05/rick-and-morty-renewed-1300x731.jpg"
-          alt="rick"
+          alt="open your friggin eyes morty"
         />
       </header>
     </section>
